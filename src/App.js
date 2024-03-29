a2e94665a1c502c8f5cbbb75ee4d45d78dcc46f7
mehdiergaibi <ergaibimehdi5@gmail.com>
2024-03-29 02:44:27 +0000

    first

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import Joke from "./components/Joke";
+
+function App() {
+  return (
+    <div className="App">
+            <h1>Joke Generator</h1>
+            <Joke/>
+        </div>
+  );
+}
+
+export default App;
